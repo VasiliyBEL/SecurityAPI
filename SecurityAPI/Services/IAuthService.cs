24c0de79729692a2f08192e0d84d673f4f7e257a
VasiliyBEL <vaspn001@gmail.com>
2023-05-16 08:08:49 +0300

    Add project files.

---
+++ SecurityAPI/Services/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using Security.Data.Models;
+
+namespace Security.Api.Services
+{
+    public interface IAuthService
+    {
+        Task<(int, string)> Registration(RegistrationModel model, string role);
+        Task<(int, string)> Login(LoginModel model);
+    }
+}
